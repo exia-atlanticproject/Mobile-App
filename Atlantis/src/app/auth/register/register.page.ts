fe80dc1c87c8f462cc09392b1810e5c8dcb24635
L-Maxence <maxence.lliteras@viacesi.fr>
2019-06-26 18:58:37 +0200

    Register pageù

--- Atlantis/src/app/auth/register/register.page.ts
+++ Atlantis/src/app/auth/register/register.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { AuthenticationService } from "../../services/authentication.service";
+import { AuthService } from "../auth.service";
 
 @Component({
   selector: "app-register",
@@ -8,12 +9,12 @@ import { AuthenticationService } from "../../services/authentication.service";
   styleUrls: ["./register.page.scss"]
 })
 export class RegisterPage implements OnInit {
-  constructor(private authenticationService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {}
 
   register(form) {
-    this.authenticationService.register(form.value).subscribe(rest => {
+    this.authService.register(form.value).subscribe(res => {
       this.router.navigateByUrl("login");
     });
   }

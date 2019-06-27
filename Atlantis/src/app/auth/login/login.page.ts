319ab3b7b73b08194e8d369a906ff4f0db1a1c2d
jdieuze <jeremy.dieuze@compta-clementine.fr>
2019-06-27 19:17:01 +0200

    :construction: OAuth webview

--- Atlantis/src/app/auth/login/login.page.ts
+++ Atlantis/src/app/auth/login/login.page.ts
@@ -1,28 +1,29 @@
-import { Component, OnInit } from "@angular/core";
-import { NavController } from "@ionic/angular";
-import { Router } from "@angular/router";
-import axios from "axios";
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import axios from 'axios';
 
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.page.html",
-  styleUrls: ["./login.page.scss"]
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
   constructor(private router: Router) {}
   ngOnInit() {
-    const stateUrl = new String(this.router.url);
+    const stateUrl = new URLSearchParams(this.router.url);
 
-    const [, code] = stateUrl.split("&code=");
-    const param = {
-      params: {
-        code
-      }
-    };
+    const params = new URLSearchParams();
+    params.append('client_id', '27fb84fe-4baf-4b6b-bfe7-f2d0638f2790');
+    params.append('client_secret', 'Zg2^04#WjA#h%6Q{]eK53J&`');
+    params.append('redirect_uri', 'urn:ietf:wg:oauth:2.0:oo');
+    params.append('grant_type', 'authorization_code');
+    params.append('code', stateUrl.get('code'));
+    params.append('scope', 'openid');
+    params.append('nonce', '1234');
+
     axios
-      .get(
-        "http://10.154.128.142:8090/login?code=eyJraWQiOiJjcGltY29yZV8wOTI1MjAxNSIsInZlciI6IjEuMCIsInppcCI6IkRlZmxhdGUiLCJzZXIiOiIxLjAifQ..mO8ETRtidYEvqfYW.fH3j_4spEFTiiEVEkO6jgOmMr8niUR4kXnJ5bMkwYJWvil_WEz5IQyEtBUpFvWCCLwz2HlGPR58jzNIi8IkpOd-3JvpNWBGOfydnIC2lsjCoRyw85uZuT8KrWYWyZuATzctF2u9SH1ct_6nVZFnNkH14YcWXn0qbEci73ocwQ9cRbBg6JPSucmgwJF8wylQ6vUHM2MvNjQdonhRJzHp7HcJfnM6BWqpZqBwVNP5Mo5t0JTSPrsz8PFrP_LjXkF-qQHp-MZgwpTPxbkvUIWf0is2y9qxatG5h3JRAu5YZpaMkrAt6MysabUKi1Cw2LtDbCLWsb9EUaufDGI3tbtx2A6Dvi8_eD3UhcHdL0mMEKWfkjA96fS3tMe5-AJvqqIhnGOR-7dZMDebNSukZdwLxbA8Z4sRI_S7x9dvOC56AX3m2JEo4PSxfLn2HXO7U8_ni72k3RjIfs_NFQjw2H1De8_u4g_V7H21J2_gyArqBUjJv64RvxEiGiLzxvRT0Pq4Yq1zrLni-3PalC5r_Ha_fO-usaEWES84-ZZE6HFKHBmHHQYgRzcOmZjS4f9oofflxgm4.SeCq_5FDUN6GCgF3idM"
-      )
+      .post('/token', params)
       .then(response => response.data)
       .then(data => {
         console.log(data);
--- Atlantis/src/app/auth/webview/webview.page.ts
+++ Atlantis/src/app/auth/webview/webview.page.ts
@@ -1,55 +1,49 @@
-import { Component, OnInit } from "@angular/core";
-import {
-  InAppBrowser,
-  InAppBrowserOptions,
-  InAppBrowserEvent
-} from "@ionic-native/in-app-browser/ngx";
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
+
+import { InAppBrowser, InAppBrowserObject } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
-  selector: "app-webview",
-  templateUrl: "./webview.page.html"
+  selector: 'app-webview',
+  templateUrl: './webview.page.html'
 })
 export class WebviewPage implements OnInit {
-  options: InAppBrowserOptions = {
-    location: "yes", //Or 'no'
-    hidden: "no", //Or  'yes'
-    clearcache: "yes",
-    clearsessioncache: "yes",
-    zoom: "yes", //Android only ,shows browser zoom controls
-    hardwareback: "yes",
-    mediaPlaybackRequiresUserAction: "no",
-    shouldPauseOnSuspend: "no", //Android only
-    closebuttoncaption: "Close", //iOS only
-    disallowoverscroll: "no", //iOS only
-    toolbar: "yes", //iOS only
-    enableViewportScale: "no", //iOS only
-    allowInlineMediaPlayback: "no", //iOS only
-    presentationstyle: "pagesheet", //iOS only
-    fullscreen: "yes" //Windows only
+  options: {
+    location: 'yes', // Or 'no'
+    hidden: 'yes', // Or  'yes'
+    clearcache: 'yes',
+    clearsessioncache: 'yes',
+    zoom: 'yes', // Android only ,shows browser zoom controls
+    hardwareback: 'yes',
+    mediaPlaybackRequiresUserAction: 'no',
+    shouldPauseOnSuspend: 'no', // Android only
+    closebuttoncaption: 'Close', // iOS only
+    disallowoverscroll: 'no', // iOS only
+    toolbar: 'yes', // iOS only
+    enableViewportScale: 'no', // iOS only
+    allowInlineMediaPlayback: 'no', // iOS only
+    presentationstyle: 'pagesheet', // iOS only
+    fullscreen: 'yes' // Windows only
   };
 
+  iab: null;
   constructor(private iab: InAppBrowser) {
-    /*let target = "_self";
-    const browser = this.iab.create(
-      "https://atlantisproject.b2clogin.com/atlantisproject.onmicrosoft.com/oauth2/v2.0/authorize?p=b2c_1_signuporsignin&response_mode=query&response_type=code&scope=openid&state=Ha2G61ljahiC1gl1oZ29hV8pyLwGAlkH56THxjEHE7KyZY8qGMciInA6nw3EmR1R&code_challenge_method=S256&code_challenge=8xaIPgOQJGea2gJGu3X0Qgku6QFWrCBE3CJ8lA5ioQY&client_id=27fb84fe-4baf-4b6b-bfe7-f2d0638f2790&redirect_uri=http%3A%2F%2Flocalhost%3A8090%2Flogin",
-      target,
-      this.options
-    );*/
+    this.iab = iab;
   }
-  ngOnInit() {
-    let target = "_self";
-    let url =
-      "https://atlantisproject.b2clogin.com/atlantisproject.onmicrosoft.com/oauth2/v2.0/authorize?p=b2c_1_signuporsignin&response_mode=query&response_type=code&scope=openid&state=Ha2G61ljahiC1gl1oZ29hV8pyLwGAlkH56THxjEHE7KyZY8qGMciInA6nw3EmR1R&code_challenge_method=S256&code_challenge=8xaIPgOQJGea2gJGu3X0Qgku6QFWrCBE3CJ8lA5ioQY&client_id=27fb84fe-4baf-4b6b-bfe7-f2d0638f2790&redirect_uri=http%3A%2F%2Flocalhost%3A8090%2Flogin";
+
+  async ngOnInit() {
+    const target = '_self';
+    const { data: url } = await axios.get('http://localhost:8090/auth');
     const browser = this.iab.create(url, target, this.options);
-    console.log("I am here");
+    console.log('I am here');
 
-    browser.on("loadstart").subscribe(e => {
-      console.log("I am here too");
+    browser.on('loadstart', e => {
+      console.log('I am here too');
 
-      if (e.url.indexOf("?code=") !== -1) {
-        let token = e.url.slice(e.url.indexOf("?code=") + "?code=".length);
+      if (e.url.indexOf('?code=') !== -1) {
+        const token = e.url.slice(e.url.indexOf('?code=') + '?code='.length);
         browser.close();
-      }
+    }
     });
   }
 }

d4ec7f633f62283de7f05102c6499944f7c96db9
L-Maxence <maxence.lliteras@viacesi.fr>
2019-06-27 17:10:35 +0200

    added axios

--- Atlantis/src/app/auth/login/login.page.ts
+++ Atlantis/src/app/auth/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { NavController } from "@ionic/angular";
 import { Router } from "@angular/router";
+import axios from "axios";
 
 @Component({
   selector: "app-login",
@@ -10,24 +11,21 @@ import { Router } from "@angular/router";
 export class LoginPage implements OnInit {
   constructor(private router: Router) {}
   ngOnInit() {
-    var stateUrl = new String(this.router.url);
-    const code = stateUrl.split("&code=");
-    console.log("Mon code = ", code[1]);
+    const stateUrl = new String(this.router.url);
 
-    /* let target = "_self";
-    let url =
-      "https://atlantisproject.b2clogin.com/atlantisproject.onmicrosoft.com/oauth2/v2.0/authorize?p=b2c_1_signuporsignin&response_mode=query&response_type=code&scope=openid&state=Ha2G61ljahiC1gl1oZ29hV8pyLwGAlkH56THxjEHE7KyZY8qGMciInA6nw3EmR1R&code_challenge_method=S256&code_challenge=8xaIPgOQJGea2gJGu3X0Qgku6QFWrCBE3CJ8lA5ioQY&client_id=27fb84fe-4baf-4b6b-bfe7-f2d0638f2790&redirect_uri=http%3A%2F%2Flocalhost%3A8090%2Flogin";
-    const browser = this.iab.create(url, target, this.options);
-    console.log("I am here");
-    browser.close();
-
-    browser.on("loadstart").subscribe(e => {
-      console.log("I am here too");
-
-      if (e.url.indexOf("?code=") !== -1) {
-        let token = e.url.slice(e.url.indexOf("?code=") + "?code=".length);
-        browser.close();
+    const [, code] = stateUrl.split("&code=");
+    const param = {
+      params: {
+        code
       }
-    });*/
+    };
+    axios
+      .get(
+        "http://10.154.128.142:8090/login?code=eyJraWQiOiJjcGltY29yZV8wOTI1MjAxNSIsInZlciI6IjEuMCIsInppcCI6IkRlZmxhdGUiLCJzZXIiOiIxLjAifQ..mO8ETRtidYEvqfYW.fH3j_4spEFTiiEVEkO6jgOmMr8niUR4kXnJ5bMkwYJWvil_WEz5IQyEtBUpFvWCCLwz2HlGPR58jzNIi8IkpOd-3JvpNWBGOfydnIC2lsjCoRyw85uZuT8KrWYWyZuATzctF2u9SH1ct_6nVZFnNkH14YcWXn0qbEci73ocwQ9cRbBg6JPSucmgwJF8wylQ6vUHM2MvNjQdonhRJzHp7HcJfnM6BWqpZqBwVNP5Mo5t0JTSPrsz8PFrP_LjXkF-qQHp-MZgwpTPxbkvUIWf0is2y9qxatG5h3JRAu5YZpaMkrAt6MysabUKi1Cw2LtDbCLWsb9EUaufDGI3tbtx2A6Dvi8_eD3UhcHdL0mMEKWfkjA96fS3tMe5-AJvqqIhnGOR-7dZMDebNSukZdwLxbA8Z4sRI_S7x9dvOC56AX3m2JEo4PSxfLn2HXO7U8_ni72k3RjIfs_NFQjw2H1De8_u4g_V7H21J2_gyArqBUjJv64RvxEiGiLzxvRT0Pq4Yq1zrLni-3PalC5r_Ha_fO-usaEWES84-ZZE6HFKHBmHHQYgRzcOmZjS4f9oofflxgm4.SeCq_5FDUN6GCgF3idM"
+      )
+      .then(response => response.data)
+      .then(data => {
+        console.log(data);
+      });
   }
 }

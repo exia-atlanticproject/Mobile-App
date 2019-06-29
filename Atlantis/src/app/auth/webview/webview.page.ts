78761fd64345b1b192b3cd3a4986454df7b5ac97
Llite <maxence.lliteras@viacesi.fr>
2019-06-30 01:31:36 +0200

    device page

--- Atlantis/src/app/auth/webview/webview.page.ts
+++ Atlantis/src/app/auth/webview/webview.page.ts
@@ -1,48 +1,84 @@
 import { Component, OnInit } from "@angular/core";
 import {
   InAppBrowser,
-  InAppBrowserOptions,
-  InAppBrowserEvent
+  InAppBrowserOptions
 } from "@ionic-native/in-app-browser/ngx";
+import { LoadingController } from "@ionic/angular";
 
 @Component({
   selector: "app-webview",
   templateUrl: "./webview.page.html"
 })
 export class WebviewPage implements OnInit {
-  options: InAppBrowserOptions = {
-    location: "yes", //Or 'no'
-    hidden: "no", //Or  'yes'
-    clearcache: "yes",
-    clearsessioncache: "yes",
-    zoom: "yes", //Android only ,shows browser zoom controls
-    hardwareback: "yes",
-    mediaPlaybackRequiresUserAction: "no",
-    shouldPauseOnSuspend: "no", //Android only
-    closebuttoncaption: "Close", //iOS only
-    disallowoverscroll: "no", //iOS only
-    toolbar: "yes", //iOS only
-    enableViewportScale: "no", //iOS only
-    allowInlineMediaPlayback: "no", //iOS only
-    presentationstyle: "pagesheet", //iOS only
-    fullscreen: "yes" //Windows only
-  };
+  url: any;
 
-  constructor(private iab: InAppBrowser) {
-    /*let target = "_self";
-    const browser = this.iab.create(
-      "https://atlantisproject.b2clogin.com/atlantisproject.onmicrosoft.com/oauth2/v2.0/authorize?p=b2c_1_signuporsignin&response_mode=query&response_type=code&scope=openid&state=Ha2G61ljahiC1gl1oZ29hV8pyLwGAlkH56THxjEHE7KyZY8qGMciInA6nw3EmR1R&code_challenge_method=S256&code_challenge=8xaIPgOQJGea2gJGu3X0Qgku6QFWrCBE3CJ8lA5ioQY&client_id=27fb84fe-4baf-4b6b-bfe7-f2d0638f2790&redirect_uri=http%3A%2F%2Flocalhost%3A8090%2Flogin",
-      target,
-      this.options
-    );*/
-  }
-  ngOnInit() {
-    let target = "_self";
+  constructor(
+    private iab: InAppBrowser,
+    public loadingCtrl: LoadingController
+  ) {
+    let target = "_system";
     let url =
       "https://atlantisproject.b2clogin.com/atlantisproject.onmicrosoft.com/oauth2/v2.0/authorize?p=b2c_1_signuporsignin&response_mode=query&response_type=code&scope=openid&state=Ha2G61ljahiC1gl1oZ29hV8pyLwGAlkH56THxjEHE7KyZY8qGMciInA6nw3EmR1R&code_challenge_method=S256&code_challenge=8xaIPgOQJGea2gJGu3X0Qgku6QFWrCBE3CJ8lA5ioQY&client_id=27fb84fe-4baf-4b6b-bfe7-f2d0638f2790&redirect_uri=http%3A%2F%2Flocalhost%3A8090%2Flogin";
+
+    const browser = this.iab.create(url, "_self");
+    browser.on("loadstop").subscribe(event => {
+      browser.insertCSS({ code: "body{color: red;" });
+    });
+
+    this.presentLoading();
+    browser.close();
+    browser.show;
+    /* browser.on("loadstart").subscribe(event => {
+      if (1 == 1) {
+        const access_token = event.url.split("&code=")[1];
+        this.presentLoading();
+        console.log(access_token);
+        browser.close();
+        return true;
+      }
+    });*/
+  }
+
+  async presentLoading() {
+    const loading = await this.loadingCtrl.create({
+      message: "Hellooo",
+      duration: 2000
+    });
+    await loading.present();
+
+    const { role, data } = await loading.onDidDismiss();
+
+    console.log("Loading dismissed!");
+  }
+
+  ngOnInit() {}
+  /* let url =
+      "https://atlantisproject.b2clogin.com/atlantisproject.onmicrosoft.com/oauth2/v2.0/authorize?p=b2c_1_signuporsignin&response_mode=query&response_type=code&scope=openid&state=Ha2G61ljahiC1gl1oZ29hV8pyLwGAlkH56THxjEHE7KyZY8qGMciInA6nw3EmR1R&code_challenge_method=S256&code_challenge=8xaIPgOQJGea2gJGu3X0Qgku6QFWrCBE3CJ8lA5ioQY&client_id=27fb84fe-4baf-4b6b-bfe7-f2d0638f2790&redirect_uri=http%3A%2F%2Flocalhost%3A8090%2Flogin";
+    console.log("I am here");
+    const browser = this.iab.create(url, target);
+
+    console.log("I am here2");
+    browser.on("loadstop").subscribe(event => {
+      if (event.url.indexOf("https://google.com")) {
+        console.log("I am here");
+      }
+      console.log("I am here 5");
+    });*/
+
+  /* let target = "_self";
+    let url =
+      "https://atlantisproject.b2clogin.com/atlantisproject.onmicrosoft.com/oauth2/v2.0/authorize?p=b2c_1_signuporsignin&response_mode=query&response_type=code&scope=openid&state=Ha2G61ljahiC1gl1oZ29hV8pyLwGAlkH56THxjEHE7KyZY8qGMciInA6nw3EmR1R&code_challenge_method=S256&code_challenge=8xaIPgOQJGea2gJGu3X0Qgku6QFWrCBE3CJ8lA5ioQY&client_id=27fb84fe-4baf-4b6b-bfe7-f2d0638f2790&redirect_uri=http%3A%2F%2Flocalhost%3A8090%2Flogin";
+    console.log("I am here");
+    cordova.plugins.
     const browser = this.iab.create(url, target, this.options);
     console.log("I am here");
+    browser.on("loadstart").subscribe(event => {
+      if (event.url.indexOf("https://atlantisproject.b2clogi") === 0) {
+        console.log("I am here");
+      }
+    });*/
 
+  /*
     browser.on("loadstart").subscribe(e => {
       console.log("I am here too");
 
@@ -50,6 +86,5 @@ export class WebviewPage implements OnInit {
         let token = e.url.slice(e.url.indexOf("?code=") + "?code=".length);
         browser.close();
       }
-    });
-  }
+    });*/
 }
